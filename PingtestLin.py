5b7ded41e0cadcd8eb43f7d27496932551386875
sushin's otovvacyber <68476834+otovvacyber@users.noreply.github.com>
2020-08-02 18:01:26 +0530

    Add files via upload

---
+++ PingtestLin.py
@@ -0,0 +1,29 @@
+'''__Author__: Sushin K'''
+'''Usage : python <script>'''
+'''Platform : Linux'''
+
+import os
+
+
+def Ping_node(hostname):
+	response = os.system("ping -c 1 " + hostname)
+
+	if response == 0:
+		print (hostname, " is up!")
+		wx = open("ReachableIP.csv", "a")
+		wx.write(hostname + "\n")
+		wx.close()
+	else:
+		print (hostname, " is down!")
+		ex = open("UnreachableIP.csv", "a")
+		ex.write(hostname + "\n")
+		ex.close()
+
+
+with open("IPList.txt", "r") as inputfile:
+        global iplist
+        iplist = inputfile.readlines()
+
+
+for i in iplist :
+        Ping_node(i.strip("\n"))
